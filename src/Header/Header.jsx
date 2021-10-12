c2b888c75d39040d39975a5d7a737558c67a3c0e
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-10-12 16:37:45 -0300

    header

---
+++ src/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => (
+  <nav className="header">
+    <h1 className="header-title">Kayser</h1>
+    <ul className="button-list">
+      <li><h1>Boton 1</h1></li>
+      <li><h1>Boton 2</h1></li>
+      <li><h1>Boton 3</h1></li>
+    </ul>
+    <div className="header-border" />
+  </nav>
+);
+
+export default Header;
