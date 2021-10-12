febd4d1c7d424c7ab6d83862ad6fbf9be05a972e
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-10-12 17:00:34 -0300

    gitflow-feature-stash: static-design

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 const Header = () => (
   <nav className="header">
-    <h1 className="header-title">Kayser</h1>
+    <h1 className="header-title">
+      Ka
+      <h1 className="header-y">y</h1>
+      ser
+    </h1>
     <ul className="button-list">
-      <li><h1>Boton 1</h1></li>
-      <li><h1>Boton 2</h1></li>
-      <li><h1>Boton 3</h1></li>
+      <li><h3>Boton 1</h3></li>
+      <li><h3>Boton 2</h3></li>
+      <li><h3>Boton 3</h3></li>
+
     </ul>
     <div className="header-border" />
   </nav>
