8583abdc962cbf94a314e60200ce75eab2615a37
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-10-13 18:45:02 -0300

    Header remake

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 
 const Header = () => (
   <nav className="header">
-    <h1 className="header-title">
-      Ka
-      <h1 className="header-y">y</h1>
-      ser
-    </h1>
+    <div className="header-card">
+      <div className="header-title">
+        <div className="blue-square" />
+        <h1 className="alphaKayser">{'AlphaKayser  '}</h1>
+      </div>
+      <h3 className="header-subtitle">Estilista</h3>
+    </div>
     <ul className="button-list">
       <li><h3>Boton 1</h3></li>
       <li><h3>Boton 2</h3></li>
       <li><h3>Boton 3</h3></li>
 
     </ul>
-    <div className="header-border" />
+    {/* <div className="header-border" /> */}
   </nav>
 );
 
